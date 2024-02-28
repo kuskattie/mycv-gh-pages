85cac0561072a7b811a0443a5947c30ef262659c
KusKattie <e.lujnova@gmail.com>
2024-02-28 21:41:51 +0100

    change in strings.tsx

--- src/Shared/Strings.tsx
+++ src/Shared/Strings.tsx
@@ -1,11 +1,11 @@
 export default{
-    ABOUT_HEADING: 'Hi! My name is Ekaterina, I am an enthusiastic, self-taught Frontend Developer with knowledge in Javacript, HTML and CSS as well as JS libraries such as React and Typescript',
-    ABOUT_DESC:'I have always shown interest in how web sites work and my husband who is a Python backend developer inspired me a lot. As a result I started learning programming to create something yourself. First I learned HTML and CSS and I was so much involved in this process that I decided to continue to deepen my knowledge. I learned JavaScript and React with the help of freeCodeCamp, watched different videos, CS50 lectures, read the documentation and practiced a lot. Now I am imporving my skills, I liked a lot Typescript and Tailwind CSS, I have got a strong desire to continue learning it professionaly.',
+    ABOUT_HEADING: 'Hi! My name is Ekaterina, I am an enthusiastic, self-taught Frontend Developer with knowledge in Javacript, HTML and CSS, Tailwind as well as JS libraries such as React and Typescript',
+    ABOUT_DESC:'I have always shown interest in how web sites work and my husband who is a Python backend developer inspired me a lot. As a result I started learning programming to create something yourself. First I learned HTML and CSS and I was so much involved in this process that I decided to continue to deepen my knowledge. I learned JavaScript and React with the help of freeCodeCamp, watched different videos, CS50 lectures, read the documentation and practiced a lot. Now I am imporving my skills, I liked a lot Typescript and Tailwind CSS, I have got a strong desire to continue learning it professionaly. Also I am a person a creative person and programming inspires me a lot to dsicover something new.',
 
     SERVICES: 'Knowledge',
 
     SKILLS:'SKills',
-    FRONT: 'JavaScript, React, Typescrypt, HTML, CSS, Tailwind CSS, jQuery',
+    FRONT: 'JavaScript, React, Typescrypt, HTML, CSS, Tailwind CSS, jQuery , GitHub, Debbuging, Photoshop, Illustrator, Procreate',
 
     LANG: 'Languages',
     LANG_DESC: 'Russian - native, English - advanced, Spanish - advanced',
